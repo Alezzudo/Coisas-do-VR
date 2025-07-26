f0c87e60e4291372f2cde550fa977d60d67aaec0
Alezzudo <153780750+Alezzudo@users.noreply.github.com>
2025-07-26 18:48:37 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,14 +1,11 @@
-// preload.js - A Ponte Segura (Atualizado para Batch e Progresso)
+// preload.js
+
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    // Agora envia um array de caminhos de arquivos
-    processFilesBatch: (filePaths) => ipcRenderer.invoke('process-files-batch', filePaths),
-    // Abre a caixa de diálogo de seleção de arquivo (agora para múltiplos)
     openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
-
-    // Eventos para receber progresso do processo principal
-    onProcessingFileProgress: (callback) => ipcRenderer.on('processing-file-progress', (_event, value) => callback(value)),
-    onProcessingOverallProgress: (callback) => ipcRenderer.on('processing-overall-progress', (_event, value) => callback(value)),
-    onProcessingBatchComplete: (callback) => ipcRenderer.on('processing-batch-complete', (_event, value) => callback(value)),
+    processFilesBatch: (filePaths) => ipcRenderer.invoke('process-files-batch', filePaths),
+    onProcessingFileProgress: (callback) => ipcRenderer.on('processing-file-progress', (event, ...args) => callback(...args)),
+    onProcessingOverallProgress: (callback) => ipcRenderer.on('processing-overall-progress', (event, ...args) => callback(...args)),
+    onProcessingBatchComplete: (callback) => ipcRenderer.on('processing-batch-complete', (event, ...args) => callback(...args)),
 });
